d169e1e11a47ffb2903916015ad29addac498f67
Geethesh0601 <geetheshr0601@gmail.com>
2023-04-17 21:25:47 +0530

    pt2-commit -final

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ CORS(app)
 @app.route("/")
 def home():
     return render_template('index.html')
+
 @app.route("/predict",methods=['POST','GET'])
 def predict():
-    try:
-        if request.method=='POST':
-            age=request.form.get('age')
-            gender=request.form.get("gender")
-            family_history=request.form.get('family_history')
-            benifits=request.form.get('benefits')
-            care_options=request.form.get('care_options')
-            anonymity=request.form.get('anonymity')
-            leave=request.form.get('leave')
-            work_interfere=request.form.get('work_interfere')
-            feature_list=np.expand_dims(np.array([age,gender,family_history,benifits,care_options,anonymity,leave,work_interfere]),axis=0)
-            feature_list=feature_list.astype(int)
-            with open('boostmodel.pkl','rb') as f:
-                model=pickle.load(f)
-            prediction=model.predict(feature_list)
-            print(prediction)
-            return "{}".format(prediction)
-        else:
-            return render_template('index.html')
-    except Exception as e:
-        print(e)
+    age = request.form.get('age')
+    gender = request.form.get('gender')
+    family_history = request.form.get('family_history')
+    benefits = request.form.get('benefits')
+    care_options = request.form.get('care_options')
+    anonymity = request.form.get('anonymity')
+    leave = request.form.get('leave')
+    work_interfere = request.form.get('work_interfere')
+
+    # Reshape input into a 2D array
+    input_data = np.array(
+        [age, gender, family_history, benefits, care_options, anonymity, leave, work_interfere]).reshape(1, -1)
+
+    # Load the model
+    with open("C:/Users/GEETHESHWAR/Downloads/boostmodel.pkl", 'rb') as f:
+        model = pickle.load(f)
+
+    # Make prediction and return result
+    prediction = model.predict(input_data)
+    return str(prediction[0])
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=1234, debug=True)
